cd134cfa2da4d9e11fbd50db0fdded7b3e4200ad
pegasis <admin@pegasis.site>
2021-02-21 21:30:48 -0500

    add more examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,82 +31,150 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Align(
         alignment: Alignment.bottomCenter,
-        child: Row(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Flexible(
-              flex: 0,
-              child: KeepKeyboardPopupMenuButton(
-                menuItemBuilder: (context, closePopup) => [
-                  KeepKeyboardPopupMenuItem(
-                    child: Text('awa'),
-                    onTap: closePopup,
-                  ),
-                  KeepKeyboardPopupMenuItem(
-                    child: Text('awa'),
-                    onTap: closePopup,
-                  ),
-                  KeepKeyboardPopupMenuItem(
-                    child: Text('awa'),
-                    onTap: closePopup,
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    filled: true,
+            Row(
+              children: [
+                Flexible(
+                  flex: 0,
+                  child: KeepKeyboardPopupMenuButton(
+                    menuItemBuilder: (context, closePopup) => [
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ),
-            Flexible(
-              flex: 0,
-              child: KeepKeyboardPopupMenuButton(
-                menuBuilder: (context, closePopup) => Container(
-                  color: Colors.teal,
-                  child: InkWell(
-                    onTap: closePopup,
-                    child: SizedBox(
-                      width: 200,
-                      height: 200,
-                      child: Text(
-                        "Custom Widget",
-                        style: TextStyle(color: Colors.white),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        filled: true,
                       ),
                     ),
                   ),
                 ),
-              ),
+                Flexible(
+                  flex: 0,
+                  child: KeepKeyboardPopupMenuButton(
+                    menuBuilder: (context, closePopup) => Container(
+                      color: Colors.teal,
+                      child: InkWell(
+                        onTap: closePopup,
+                        child: SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Text(
+                            "Custom Menu Widget",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Flexible(
-              flex: 0,
-              child: WithKeepKeyboardPopupMenu(
-                menuItemBuilder: (context, closePopup) => [
-                  KeepKeyboardPopupMenuItem(
-                    child: Text('awa'),
-                    onTap: closePopup,
+            SizedBox(
+              height: 48,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  WithKeepKeyboardPopupMenu(
+                    menuItemBuilder: (context, closePopup) => [
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                    ],
+                    childBuilder: (context, openPopup) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: openPopup,
+                        child: Text('Custom Trigger'),
+                      ),
+                    ),
                   ),
-                  KeepKeyboardPopupMenuItem(
-                    child: Text('awa'),
-                    onTap: closePopup,
+                  WithKeepKeyboardPopupMenu(
+                    menuItemBuilder: (context, closePopup) => [
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                    ],
+                    childBuilder: (context, openPopup) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: openPopup,
+                        child: Text('Custom Background'),
+                      ),
+                    ),
+                    backgroundBuilder: (context, child) => Material(
+                      elevation: 16,
+                      borderRadius: BorderRadius.circular(16),
+                      shadowColor: Colors.red,
+                      color: Colors.grey,
+                      child: child,
+                    ),
                   ),
-                  KeepKeyboardPopupMenuItem(
-                    child: Text('awa'),
-                    onTap: closePopup,
+                  WithKeepKeyboardPopupMenu(
+                    menuItemBuilder: (context, closePopup) => [
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                      KeepKeyboardPopupMenuItem(
+                        child: Text('awa'),
+                        onTap: closePopup,
+                      ),
+                    ],
+                    childBuilder: (context, openPopup) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: openPopup,
+                        child: Text('Custom position'),
+                      ),
+                    ),
+                    calculateMenuPosition:
+                        (Size menuSize, Rect overlayRect, Rect buttonRect) {
+                      return Offset(8, 8);
+                    },
                   ),
                 ],
-                childBuilder: (context, openPopup) {
-                  return ElevatedButton(
-                    onPressed: openPopup,
-                    child: Text('Button'),
-                  );
-                },
               ),
-            )
+            ),
           ],
         ),
       ),
